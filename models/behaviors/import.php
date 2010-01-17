d736c8052b5d244056c19fa6421601276a6ed10d
Dean Sofer <Dean@DeanSofer-Mac.local>
2010-01-17 03:12:30 -0800

    Initial Commit

---
+++ models/behaviors/import.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * CSV Import Behavior
+ * 
+ * Based on a model function by ''
+ * http://www.com/
+ *
+ * @author Dean Sofer (proloser@hotmail.com)
+ * @package app
+ * @subpackage app.models.behaviors
+ * @filesource http://github.com/josegonzalez/MeioUpload/tree/master
+ * @version $Id$
+ * @copyright __MyCompanyName__, 13 January, 2010
+ * @package default
+ **/
+
+/**
+ * Define DocBlock
+ **/
+
+class ImportBehavior extends ModelBehavior {
+	
+	/**
+	 * undocumented function
+	 *
+	 * @param string $filename 
+	 * @return void
+	 * @author Dean Sofer
+	 */
+	
+}
+
+?>
